427e3f5f671bb015e91d6197f354f20f89e6a214
AymanDoukali <aym99699@gmail.com>
2024-03-12 11:25:44 +0100

    Ayman

--- Hello_world.py
+++ Hello_world.py
@@ -2,11 +2,12 @@
 
 # This file produce some printing
 
-print("Hello world")
+print("Hello world Ayman")
 
 
 # We introduce some interaction with the user
 
 name = input("What's your name?")
 print("\nHello " + name)
+
 
